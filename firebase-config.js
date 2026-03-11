86e86a048dc417d834bb5633fbc351fc098cccdc
Miklens <rnd@miklensbio.com>
2026-03-11 22:19:30 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,10 +13,7 @@
     storageBucket: 'inventory-management-d2ace.firebasestorage.app',
     messagingSenderId: '79940113871',
     appId: '1:79940113871:web:fbf769a8ca5999e6296eba',
-    // Free email via Apps Script (no Blaze). Paste script URL after deploy. See docs/EMAIL-FREE-APPS-SCRIPT.md
-    APP_SCRIPT_EMAIL_URL: 'https://script.google.com/macros/s/AKfycbw0BE0REK2Oj51j--347PtyyHZfvtUYDIZLc7Ts1CEpBVgrEcbtLhgLIyxJcuF9oRkQkw/exec',
-    APP_SCRIPT_EMAIL_SECRET: 'MiklensEmailSecret2024XyZ789',
-    APP_URL: 'https://miklens.github.io/Inventory-management'
+    APP_URL: 'https://miklens.github.io/inventory-management-2.0/'
   };
   if (typeof global !== 'undefined') global.FIREBASE_CONFIG = config;
   if (typeof window !== 'undefined') window.FIREBASE_CONFIG = config;
